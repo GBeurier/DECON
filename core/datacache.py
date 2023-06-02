fc9db06a508225aa11d93203813b7dd20169c541
Gregory Beurier <beurier@cirad.fr>
2023-06-02 14:31:00 +0200

    updated refactoring

--- core/datacache.py
+++ core/datacache.py
@@ -1,10 +1,15 @@
 import csv
+from functools import reduce
 import gzip
-from pathlib import Path
-import re
-
+import logging
 import numpy as np
 import pandas as pd
+from pathlib import Path
+import traceback
+import re
+
+
+
 
 
 DATACACHE = {}
@@ -58,64 +63,80 @@ def get_properties(file_path):
     
     sniffer = csv.Sniffer()
     dialect = sniffer.sniff(second_line)
-    # print(dialect.delimiter)
+    # logging.info(dialect.delimiter)
     return header, dialect.delimiter
 
 
 def load_csv(file_path):
     """
-    This function loads a CSV file, removes missing values, and converts the data to a numpy array.
-
+    This function loads a CSV file, infers its properties, corrects the delimiter if necessary, reads
+    the file into a pandas dataframe, cleans the data by dropping empty columns and rows, removes rows
+    with missing values, and returns the cleaned data as a numpy array along with a list of rows that
+    were removed.
+    
     :param file_path: The file path of the CSV file to be loaded
-    :return: a numpy array containing the data from the CSV file after cleaning and processing.
+    :return: a tuple containing the loaded data as a numpy array and a list of row indices that contain
+    missing values.
     """
-    print("Loading file:", file_path)
+    logging.info("Loading file: %s", file_path)
     data = None
     header, delimiter = get_properties(file_path)
-    print("Header inference:", header)
-    print("Delimiter inference:", delimiter)
+    logging.info("Header inference: %s", header)
+    logging.info("Delimiter inference: %s", delimiter)
     if re.search(r"[0-9\.]", delimiter):
         delimiter = "\n"
-        print("Delimiter correction: \\n")
+        logging.warning("Delimiter correction: \\n")
     try:
         if header:
             data = pd.read_csv(file_path, header=0, na_filter=False, sep=delimiter, engine='python')
         else:
             data = pd.read_csv(file_path, header=None, na_filter=False, sep=delimiter, engine='python')
     except Exception as e:
-        print(e)
+        logging.error("Error loading file: %s", file_path)
+        logging.error("Exception %s", e)
+        logging.error(traceback.format_exc())
         return None
 
     data = data.replace(r"^\s*$", "", regex=True)
     data = data.apply(pd.to_numeric, args=("coerce",))
 
-    print("Data shape:", data.shape)
+    logging.info("Data shape: %s", data.shape)
     data = data.dropna(how='all', axis=1)
-    print("Data shape after dropna(all) cols:", data.shape)
+    logging.info("Data shape after dropna(all) cols: %s", data.shape)
     data = data.dropna(how='all', axis=0)
-    print("Data shape after dropna(all) rows:", data.shape)
+    logging.info("Data shape after dropna(all) rows: %s", data.shape)
 
     na_coords = np.argwhere(pd.isna(data).values)
     na_row_list = []
     if len(na_coords) > 0:
         na_row_list = list(set(na_coords[:, 0]))
-        print("Removing rows:", na_row_list)
-        data = data.dropna(how='any', axis=0)
-        print("Data shape after dropna(any) rows:", data.shape)
+        logging.info("Rows to remove: %s", na_row_list)
+        # data = data.dropna(how='any', axis=0)
+        # logging.info("Data shape after dropna(any) rows:", data.shape)
 
     data = data.astype(np.float32).values
-    print(data[:2, :2])
+    logging.info("data_sample:\n%s", data[:2, :2])
 
     return data, na_row_list
 
 
 def register_dataset(dataset_config):
+    """
+    This function registers a dataset by loading and processing its X and y files, removing rows with
+    missing values, and storing the resulting data in a cache.
+    
+    :param dataset_config: The configuration of the dataset to be registered. It can be either a string
+    representing the path to the dataset directory or a tuple containing the path to the dataset
+    directory and the index of the column containing the target variable
+    :return: a tuple containing the cache hash, dataset name, and cache.
+    """
     y_files = [("y_train", "*ycal*"), ("y_test", "*ytest*"), ("y_val", "*yval*")]
     X_files = [("X_train", "*Xcal*"), ("X_test", "*Xtest*"), ("X_val", "*Xval*")]
+    files = X_files.copy()
 
     dataset_path = ""
     if isinstance(dataset_config, str):
-        X_files.extend(y_files)
+        files.extend(y_files)
         dataset_path = dataset_config
     elif isinstance(dataset_config, tuple):
         dataset_path, y_col = dataset_config
@@ -132,22 +153,63 @@ def register_dataset(dataset_config):
         "y_val": None
     }
 
-    for key, pattern in X_files:
+    removed_rows = {
+        "X_train": [],
+        "X_test": [],
+        "X_val": [],
+        "y_train": [],
+        "y_test": [],
+        "y_val": []
+    }
+
+    for key, pattern in files:
         files = list(dataset_dir.glob(pattern))
         if len(files) > 1:
-            cache[key] = tuple(load_csv(f) for f in files if f.is_file())
+            csv_list = [load_csv(f) for f in files if f.is_file()]
+            cache[key] = tuple(x[0] for x in csv_list)
+            removed_rows[key] = tuple(x[1] for x in csv_list)
         elif len(files) == 1:
-            cache[key] = load_csv(files[0])
+            if files[0].is_file():
+                cache[key], removed_rows[key] = load_csv(files[0])
 
+
+    # Get y from a col of X
     if isinstance(dataset_path, tuple):
-        for i, (key, pattern) in enumerate(y_files):
+        for i, (y_key, pattern) in enumerate(y_files):
             x_key = X_files[i][0]
             if isinstance(cache[x_key], tuple):
-                cache[key] = tuple(cache[x_key][j][:, y_col] for j in range(len(cache[x_key])))
-                cache[x_key] = tuple(np.delete(cache[x_key][j], y_col, axis=1) for j in range(len(cache[x_key])))
+                cache[y_key] = tuple(cache[x_key][j][:, y_col] for j in range(len(cache[x_key])))
+                # cache[x_key] = tuple(np.delete(cache[x_key][j], y_col, axis=1) for j in range(len(cache[x_key])))
             else:
-                cache[key] = cache[x_key][:, y_col]
-                cache[x_key] = np.delete(cache[x_key], y_col, axis=1)
+                cache[y_key] = cache[x_key][:, y_col]
+                # cache[x_key] = np.delete(cache[x_key], y_col, axis=1)
+    
+
+
+    # Remove rows with missing values
+    for i, (y_key, pattern) in enumerate(y_files):
+            x_key = X_files[i][0]
+            if isinstance(cache[x_key], tuple):
+                indexes_to_remove = reduce(np.union1d, ([removed_rows[x_key][j] for j in range(len(removed_rows[x_key]))] + [removed_rows[y_key][j] for j in range(len(removed_rows[y_key]))])).astype(np.int32)
+                if indexes_to_remove.shape[0] > 0:
+                    logging.warning("Removing rows: %s", str(indexes_to_remove))
+                    cache[x_key] = tuple(np.delete(cache[x_key][j], indexes_to_remove, axis=0) for j in range(len(cache[x_key])))
+                    cache[y_key] = tuple(np.delete(cache[y_key][j], indexes_to_remove, axis=0) for j in range(len(cache[y_key])))
+            else:
+                indexes_to_remove = np.union1d(removed_rows[x_key], removed_rows[y_key]).astype(np.int32)
+                if indexes_to_remove.shape[0] > 0:
+                    logging.warning("Removing rows: %s", str(indexes_to_remove))
+                    cache[x_key] = np.delete(cache[x_key], indexes_to_remove, axis=0)
+                    cache[y_key] = np.delete(cache[y_key], indexes_to_remove, axis=0)
+
+    # check if cache X and y are consistent
+    for i, (y_key, pattern) in enumerate(y_files):
+        x_key = X_files[i][0]
+        if isinstance(cache[x_key], tuple):
+            for j in range(len(cache[x_key])):
+                assert (cache[x_key][j] is None and cache[y_key][j] is None) or (cache[x_key][j].shape[0] == cache[y_key][j].shape[0])
+        else:
+            assert (cache[x_key] is None and cache[y_key] is None) or (cache[x_key].shape[0] == cache[y_key].shape[0])
 
     cache_hash = data_hash([cache[key] for key in sorted(cache.keys())])
     cache["path"] = dataset_path
@@ -216,7 +278,7 @@ def register_dataset(dataset_config):
 #     if autoremove_na:
 #         if np.isnan(x_data).any():
 #             x_rows_del, _ = np.where(np.isnan(x_data))
-#             print("Missing X:", x_rows_del)
+#             logging.info("Missing X:", x_rows_del)
 #             x_data = np.delete(x_data, x_rows_del, axis=0)
 
 #     if len(x_data.shape) != 2 or len(x_data) == 0:
@@ -236,9 +298,9 @@ def register_dataset(dataset_config):
 
 #             if np.isnan(y_data).any():
 #                 y_rows_del, _ = np.where(np.isnan(y_data))
-#                 # print("Missing Y:", y_rows_del)
+#                 # logging.info("Missing Y:", y_rows_del)
 
-#                 # print("NULLL", np.where(np.isnull(y_data)))
+#                 # logging.info("NULLL", np.where(np.isnull(y_data)))
 #                 y_data = np.delete(y_data, y_rows_del, axis=0)
 #                 x_data = np.delete(x_data, y_rows_del, axis=0)
 
@@ -266,7 +328,7 @@ def register_dataset(dataset_config):
 #         x_df = x_df.replace(r"^\s*$", np.nan, regex=True).apply(pd.to_numeric, args=("coerce",))
 #         x_data.append(x_df.astype(np.float32).values)
 #     x_data = np.array(x_data)
-#     print(x_data.shape)
+#     logging.info(x_data.shape)
 
 #     x_rows_del = []
 #     if autoremove_na:
@@ -274,11 +336,11 @@ def register_dataset(dataset_config):
 #             if np.isnan(x_data[i]).any():
 #                 x_del, _ = np.where(np.isnan(x_data[i]))
 #                 x_rows_del = np.union1d(x_rows_del, x_del)
-#                 print("Missing X:", x_rows_del)
+#                 logging.info("Missing X:", x_rows_del)
 #         x_data = np.delete(x_data, x_rows_del, axis=1)
 
 #     if len(x_rows_del) > 0:
-#         print("X rows deleted: ", x_rows_del)
+#         logging.info("X rows deleted: ", x_rows_del)
 
 #     if len(x_data.shape) != 3 or len(x_data[0]) == 0:
 #         raise WrongFormatError(x_data, None)
@@ -300,7 +362,7 @@ def register_dataset(dataset_config):
 #                 y_data = np.delete(y_data, y_rows_del, axis=0)
 #                 x_data = np.delete(x_data, y_rows_del, axis=1)
 #                 if len(y_rows_del) > 0:
-#                     print("Y rows deleted: ", y_rows_del)
+#                     logging.info("Y rows deleted: ", y_rows_del)
 
 #         if len(y_data.shape) != 2:
 #             raise WrongFormatError(x_data, y_data)
@@ -319,7 +381,7 @@ def register_dataset(dataset_config):
 # def load_data(path, resampling=None, resample_size=0):
 
 #     if resampling is not None:
-#         print('(', resampling, resample_size, ')', end=" ")
+#         logging.info('(', resampling, resample_size, ')', end=" ")
 
 #     projdir = Path(path)
 #     files = tuple(next(projdir.glob(n)) for n in ["*Xcal*", "*Ycal*"])
@@ -356,7 +418,7 @@ def register_dataset(dataset_config):
 # def load_data_multiple(path, resampling=None, resample_size=0):
 
 #     if resampling is not None:
-#         print('(', resampling, resample_size, ')', end=" ")
+#         logging.info('(', resampling, resample_size, ')', end=" ")
 
 #     projdir = Path(path)
     
